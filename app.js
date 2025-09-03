7c58b888c18f9695c1e728829d24cf9223a39ed6
Daniela Carrillo <danlestwalker@gmail.com>
2025-09-02 23:53:57 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -6,17 +6,32 @@ input.addEventListener("keypress", function (event) {
   }
 });
 
-// agrega amigos a la lista
+// Crear un array para almacenar los nombres
 let amigos = [];
 function agregarAmigo() {
+  // Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío
   const nombre = input.value.trim();
-  // si el nombre es vacio se manda una alerta y no agrega nada
+  // Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
   if (nombre === "") {
     alert("Por favor, inserte un nombre.");
     return;
   }
-  // si no es vacio agrega el nombre a la lista
+
   amigos.push(nombre);
   mostrarAmigos();
+  // Limpiar la lista existente
   input.value = "";
-}// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+}
+// Función que recorre el array y actualiza la lista HTML
+function mostrarAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  // Limpiar la lista existente
+  lista.innerHTML = "";
+  // Iterar sobre el arreglo
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    // Agregar elementos a la lista
+    lista.appendChild(li);
+  }
+}
